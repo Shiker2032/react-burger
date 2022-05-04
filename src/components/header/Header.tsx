c0a33772ccbb43cb354356ce382bb8131bd5bf03
Vladislav <53581923+Shiker2032@users.noreply.github.com>
2022-05-04 19:28:27 +0300

    закончил header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -12,20 +12,25 @@ export default function Header() {
     <div className={styles.header}>
       <nav>
         <ul className={styles.navigation}>
+          <ul className={styles.navigation__menu}>
+            <li className={styles.navigation__item}>
+              <BurgerIcon type="primary" />
+              <p className="text text_type_main-default">Конструктор</p>
+            </li>
+            <li className={styles.navigation__item}>
+              <ListIcon type="secondary" />
+              <p className="text text_type_main-default text_color_inactive">
+                Лента заказов
+              </p>
+            </li>
+          </ul>
           <li className={styles.navigation__item}>
-            <BurgerIcon type="primary" />
-            <p className="text text_type_main-default">Конструктор</p>
-          </li>
-          <li className={styles.navigation__item}>
-            <ListIcon type="secondary" />
-            <p className="text text_type_main-default text_color_inactive">
-              Лента заказов
-            </p>
-          </li>
-          <li style={{ width: "280px" }} className={styles.navigation__item}>
             <Logo />
           </li>
-          <li className={styles.navigation__item}>
+          <li
+            className={styles.navigation__item}
+            style={{ justifySelf: "end" }}
+          >
             <ProfileIcon type="secondary" />
             <p className="text text_type_main-default text_color_inactive">
               Личный кабинет
