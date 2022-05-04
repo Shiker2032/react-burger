623357090c67d61604f92da1774e61035fa82aed
Vladislav <53581923+Shiker2032@users.noreply.github.com>
2022-05-04 16:15:49 +0300

    Первая версия сайта

--- src/components/BurgerIngridients.jsx
+++ src/components/BurgerIngridients.jsx
@@ -3,6 +3,8 @@ import BurgerIngridient from "./BurgerIngridient";
 import {
   Tab,
   ConstructorElement,
+  CurrencyIcon,
+  Button,
 } from "@ya.praktikum/react-developer-burger-ui-components";
 
 export default function BurgerIngridients({ data }) {
@@ -14,14 +16,29 @@ export default function BurgerIngridients({ data }) {
             return <BurgerIngridient bun={bun} />;
           })}
         </div>
-        <div className="constructor">
-          <ConstructorElement
-            type="top"
-            isLocked={true}
-            text="Краторная булка N-200i (верх)"
-            price={200}
-            thumbnail={data[0].image}
-          />
+        <div className="constructor-wrapper">
+          <div className="constructor">
+            <div className="burger-components">
+              {data.map((element, idx) => {
+                return (
+                  <ConstructorElement
+                    type="top"
+                    isLocked={true}
+                    text={element.name}
+                    price={element.price}
+                    thumbnail={element.image}
+                  />
+                );
+              })}
+            </div>
+          </div>
+          <div className="order-info">
+            <p className="text text_type_digits-medium">1238</p>
+            <CurrencyIcon type="primary" />
+            <Button type="primary" size="medium">
+              Оформить заказ
+            </Button>
+          </div>
         </div>
       </div>
     </>
