d8fbb7c6614b6be1d99d881d015bc785a7e914d1
Vladislav <53581923+Shiker2032@users.noreply.github.com>
2022-05-06 21:27:55 +0300

    Добавил драг иконку

---
+++ src/components/API/api.jsx
@@ -0,0 +1,5 @@
+const config = {
+  url: "https://norma.nomoreparties.space/api/ingredients", //ссылка
+  headers: {},
+};
+export default config;
--- src/components/burger-constructor/BurgerConstructor.jsx
+++ src/components/burger-constructor/BurgerConstructor.jsx
@@ -3,28 +3,34 @@ import {
   ConstructorElement,
   Button,
   CurrencyIcon,
+  DragIcon,
 } from "@ya.praktikum/react-developer-burger-ui-components";
 function BurgerConstructor({ data }) {
   return (
     <>
-      <div className="burgerSelect">
-        <div className="constructor-wrapper">
-          <div className="constructor">
-            <div className="burger-components">
-              {data.map((element, idx) => {
-                return (
-                  <ConstructorElement
-                    key={element._id}
-                    type="top"
-                    isLocked={true}
-                    text={element.name}
-                    price={element.price}
-                    thumbnail={element.image}
-                  />
-                );
-              })}
-            </div>
-          </div>
+      <section className="burgerSelect">
+        <ul className="constructor-wrapper">
+          <li className="constructor">
+            {data.map((element, idx) => {
+              return (
+                <>
+                  <article className="burger-components">
+                    <p>
+                      <DragIcon type="primary" />
+                    </p>
+                    <ConstructorElement
+                      key={element._id}
+                      type="top"
+                      isLocked={false}
+                      text={element.name}
+                      price={element.price}
+                      thumbnail={element.image}
+                    />
+                  </article>
+                </>
+              );
+            })}
+          </li>
           <div className="order-info">
             <div className="order-info__price">
               <p className="text text_type_digits-medium">1238</p>
@@ -34,8 +40,8 @@ function BurgerConstructor({ data }) {
               Оформить заказ
             </Button>
           </div>
-        </div>
-      </div>
+        </ul>
+      </section>
     </>
   );
 }
