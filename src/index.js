9a3803a6c60a1831de23a2ef6f14b1350a13af4c
Vladislav <53581923+Shiker2032@users.noreply.github.com>
2022-06-25 18:19:07 +0300

    Страница регистрации

--- src/index.js
+++ src/index.js
@@ -7,14 +7,18 @@ import { store } from "./services/store";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./pages/Login";
+import Registration from "./pages/Registration";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <Router>
       <Switch>
-        <Route path="/">
+        <Route path="/login">
           <Login />
+        </Route>
+        <Route path="/">
+          <Registration />
         </Route>
       </Switch>
     </Router>
---
+++ src/pages/Registration.js
@@ -0,0 +1,57 @@
+import styles from "./registration.module.css";
+
+import {
+  Input,
+  PasswordInput,
+  Button,
+} from "@ya.praktikum/react-developer-burger-ui-components";
+import Header from "../components/Header/Header";
+import { NavLink } from "react-router-dom";
+import { useState } from "react";
+
+function Registration(props) {
+  const [nameInput, setNameInput] = useState("");
+  const [emailInput, setEmailinput] = useState("");
+  const [passwordInput, setPasswordInput] = useState("");
+  return (
+    <>
+      <Header />
+
+      <main className={styles.login__wrapper}>
+        <div className={styles.login__body}>
+          <p className="text text_type_main-medium mb-6">Регистрация</p>
+          <div className="pb-6">
+            <Input
+              type="text"
+              onChange={(e) => setNameInput(e.target.value)}
+              value={nameInput}
+              placeholder="Имя"
+            />
+          </div>
+          <div className="pb-6">
+            <Input
+              type="email"
+              onChange={(e) => setEmailinput(e.target.value)}
+              value={emailInput}
+              placeholder="E-mail"
+            />
+          </div>
+          <PasswordInput />
+          <div className="pt-6">
+            <Button type="primary" size="small ">
+              зарегистрироваться
+            </Button>
+          </div>
+          <p className="text text_type_main-default text_color_inactive pt-20">
+            Уже зарегистрированы?
+            <NavLink className={styles.login__link} to="/">
+              Войти
+            </NavLink>
+          </p>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default Registration;
